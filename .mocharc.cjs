f4dede6e1e25925a23c70abe87a596f3c44c3067
harcop <harcopdrew@gmail.com>
2022-07-05 11:12:44 +0100

    disabled junit report

--- .mocharc.cjs
+++ .mocharc.cjs
@@ -4,10 +4,10 @@ module.exports = {
   recursive: true,
   timeout: 5000,
   // Write JUnit XML report when we're running in CI
-  ...(process.env.CI
-    ? {
-        reporter: 'mocha-junit-reporter',
-        'reporter-option': `mochaFile=./tmp/tests/junit-${Date.now()}.xml`,
-      }
-    : {}),
+  // ...(process.env.CI
+  //   ? {
+  //       reporter: 'mocha-junit-reporter',
+  //       'reporter-option': `mochaFile=./tmp/tests/junit-${Date.now()}.xml`,
+  //     }
+  //   : {}),
 }
