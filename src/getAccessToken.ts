c136072c3d237bab83dfefa0440ff8153452d201
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2022-07-11 16:35:10 +0100

    Read token response

--- src/getAccessToken.ts
+++ src/getAccessToken.ts
@@ -1,71 +1,57 @@
 import http from 'http'
+import https from 'https'
 
-export async function getAccessToken(baseUrl: string, refreshToken: string): Promise<string> {
-  const authHeaders: http.OutgoingHttpHeaders = {
+import { readStream } from './readStream.js'
+
+export async function getAccessToken(
+  baseUrl: string,
+  refreshToken: string,
+  requestTimeout?: number
+): Promise<string> {
+  const headers: http.OutgoingHttpHeaders = {
     Authorization: `Bearer ${refreshToken}`,
   }
-
   const url = new URL(`/api/refresh-access-token`, baseUrl)
 
-  //   let h: typeof http | typeof https
-  //   switch (url.protocol) {
-  //     case 'http:':
-  //       h = http
-  //       break
-  //     case 'https:':
-  //       h = https
-  //       break
-  //     default:
-  //       return reject(new Error(`Unsupported protocol: ${url.toString()}`))
-  //   }
-  //   const headers = {
-  //     'Content-Type': contentTypes[extname(path) as Extension],
-  //     'Content-Length': stat.size,
-  //     ...(ciEnv?.git?.remote ? { 'OneReport-SourceControl': ciEnv.git.remote } : {}),
-  //     ...(ciEnv?.git?.revision ? { 'OneReport-Revision': ciEnv.git.revision } : {}),
-  //     ...(ciEnv?.git?.branch ? { 'OneReport-Branch': ciEnv.git.branch } : {}),
-  //     ...(ciEnv?.git?.tag ? { 'OneReport-Tag': ciEnv.git.tag } : {}),
-  //     ...authHeaders,
-  //   }
+  return new Promise<string>((resolve, reject) => {
+    let h: typeof http | typeof https
+    switch (url.protocol) {
+      case 'http:':
+        h = http
+        break
+      case 'https:':
+        h = https
+        break
+      default:
+        return reject(new Error(`Unsupported protocol: ${url.toString()}`))
+    }
 
-  //   const req = h.request(
-  //     url.toString(),
-  //     {
-  //       method: 'POST',
-  //       headers,
-  //     },
-  //     (res) => {
-  //       readStream(res)
-  //         .then((buffer) => buffer.toString('utf-8'))
-  //         .then((body) => {
-  //           if (res.statusCode !== 201) {
-  //             return reject(
-  //               new Error(`Unexpected status code ${res.statusCode}
-  // POST ${url.toString()} -d @${path}
-  // > ${Object.entries(headers)
-  //                 .map(([h, v]) => `${h}: ${v}`)
-  //                 .join('\n> ')}
-
-  // < ${Object.entries(res.headers)
-  //                 .map(([h, v]) => `${h}: ${v}`)
-  //                 .join('\n< ')}
-
-  // ${body}
-  // `)
-  //             )
-  //           } else {
-  //             try {
-  //               const responseBody = JSON.parse(body) as ResponseBody
-  //               return resolve(responseBody)
-  //             } catch (err) {
-  //               reject(err)
-  //             }
-  //           }
-  //         })
-  //         .catch(reject)
-  //     }
-  //   )
-
-  throw new Error('invalid token')
-  // return 'access token'
+    const req = h.request(
+      url.toString(),
+      {
+        method: 'POST',
+        headers,
+      },
+      (res) => {
+        if (res.statusCode !== 201) {
+          return reject(new Error(`Invalid refresh token`))
+        } else {
+          readStream(res)
+            .then((buffer) => buffer.toString('utf-8'))
+            .then((body) => {
+              const ob = JSON.parse(body)
+              resolve(ob.accessToken)
+            })
+        }
+      }
+    )
+    if (requestTimeout) {
+      req.setTimeout(requestTimeout)
+    }
+    req.on('error', reject)
+    req.on('timeout', () =>
+      reject(new Error(`request to ${url.toString()} timed out after ${requestTimeout}ms`))
+    )
+    req.end()
+  })
 }
