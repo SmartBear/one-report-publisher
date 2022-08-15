afecc397681431546ef4323ade6dcd56e22b37ea
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2022-08-15 09:57:54 -0500

    Revert windows conversion

--- src/manyglob.ts
+++ src/manyglob.ts
@@ -1,18 +1,16 @@
 import fg from 'fast-glob'
 import * as os from 'os'
-import path from 'path'
-
-const normalize = path.win32.normalize
 
 export async function manyglob(globs: readonly string[]): Promise<readonly string[]> {
   return (
     await Promise.all(
       globs.reduce<readonly Promise<string[]>[]>((prev, glob) => {
-        const path = normalize(glob.replace(/^~/, os.homedir()))
-        console.log(path, 'this is the path')
-        const globPath = fg(path)
-        console.log(globPath, 'this is the globPath')
-        return prev.concat(globPath)
+        console.log(glob, 'this is the glob')
+        const globWithHomeDirExpanded = glob.replace(/^~/, os.homedir())
+        console.log(globWithHomeDirExpanded, 'this is the glob with homedir expanded')
+        const foundPaths = fg(globWithHomeDirExpanded)
+        console.log(foundPaths, 'this is the found paths')
+        return prev.concat(foundPaths)
       }, [])
     )
   ).flatMap((paths) => paths)
