48a2ae1c6c6b26191030627b794a5f80a89f27b5
harcop <harcopdrew@gmail.com>
2022-08-15 11:56:53 +0100

    Add log to manyglob

--- src/manyglob.ts
+++ src/manyglob.ts
@@ -4,11 +4,13 @@ import path from 'path'
 const normalize = process.platform === 'win32' ? path.win32.normalize : path.normalize
 
 export async function manyglob(globs: readonly string[]): Promise<readonly string[]> {
-  return (
+  const globPaths = (
     await Promise.all(
       globs.reduce<readonly Promise<string[]>[]>((prev, glob) => {
         return prev.concat(fg(normalize(glob.replace(/^~/, os.homedir()))))
       }, [])
     )
   ).flatMap((paths) => paths)
+  console.log(globPaths, 'globPaths', globs, 'globs')
+  return globPaths
 }
