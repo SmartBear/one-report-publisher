3e2e57793484453259db9800a9cd0cd0ed688ef7
harcop <harcopdrew@gmail.com>
2022-08-15 11:49:18 +0100

    Update normalize for windows

--- src/manyglob.ts
+++ src/manyglob.ts
@@ -1,12 +1,13 @@
 import fg from 'fast-glob'
 import * as os from 'os'
 import path from 'path'
+const normalize = process.platform === 'win32' ? path.win32.normalize : path.normalize
 
 export async function manyglob(globs: readonly string[]): Promise<readonly string[]> {
   return (
     await Promise.all(
       globs.reduce<readonly Promise<string[]>[]>((prev, glob) => {
-        return prev.concat(fg(path.normalize(glob.replace(/^~/, os.homedir()))))
+        return prev.concat(fg(normalize(glob.replace(/^~/, os.homedir()))))
       }, [])
     )
   ).flatMap((paths) => paths)
