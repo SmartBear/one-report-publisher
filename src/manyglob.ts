16cd8fea1b559b0f4a7abbabc8a3c15cf3c6764a
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2022-08-15 10:03:09 -0500

    Use forward slashes even on windows

--- src/manyglob.ts
+++ src/manyglob.ts
@@ -1,12 +1,14 @@
 import fg from 'fast-glob'
 import * as os from 'os'
+import path from 'path'
 
 export async function manyglob(globs: readonly string[]): Promise<readonly string[]> {
   return (
     await Promise.all(
       globs.reduce<readonly Promise<string[]>[]>((prev, glob) => {
         console.log(glob, 'this is the glob')
-        const globWithHomeDirExpanded = glob.replace(/^~/, os.homedir())
+        const homeDir = os.homedir().split(path.sep).join(path.posix.sep)
+        const globWithHomeDirExpanded = glob.replace(/^~/, homeDir)
         console.log(globWithHomeDirExpanded, 'this is the glob with homedir expanded')
         const foundPaths = fg(globWithHomeDirExpanded)
         console.log(foundPaths, 'this is the found paths')
